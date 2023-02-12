c191c3afb4476a2353272f87c569efd5a1681776
brake <riaconoo@icloud.com>
2023-02-12 14:35:22 +1100

    created service for api call - data logs to console

--- geek-flix/src/app/app.component.ts
+++ geek-flix/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MoviesService } from './movies.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'geek-flix';
+
+  constructor(private movie:MoviesService) {
+    this.movie.getData().subscribe(data => {
+      console.warn(data)
+    })
+  }
 }
