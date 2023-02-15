1bb3ca4074f3af757c559fdccb7b738e0bae7cee
brake <riaconoo@icloud.com>
2023-02-15 22:30:04 +1100

    updated data convert from parent to child component

--- geek-flix/src/app/home-card/home-card.component.ts
+++ geek-flix/src/app/home-card/home-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { tap } from 'rxjs';
 import { MovieDataService } from '../services/movie-data.service';
 
@@ -12,12 +12,30 @@ export class HomeCardComponent {
   movieData: any = {};
   constructor(private movieDataService: MovieDataService) {}
 
+  @Output() dataEvent = new EventEmitter<string>();
+
+  data: string = 'This data was passed';
+
+  sendData() {
+    this.dataEvent.emit(this.data);
+    console.log(this.data);
+  }
+
+  set_id(id: number) {
+    console.log(id);
+  }
+
+
+  
+
   ngOnInit(): void {
     this.movieDataService.getData().subscribe((data) => {
       this.movieData = data;
       // JSON to console
       console.warn(data);
     })
+    
+
   }
   
 
--- geek-flix/src/app/info-page/info-page.component.ts
+++ geek-flix/src/app/info-page/info-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieDataService } from '../services/movie-data.service';
 
 @Component({
   selector: 'info-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-page.component.css']
 })
 export class InfoPageComponent {
+  
+  data: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  receiveData(data: string) {
+    this.data = data;
+  }
 
 }
