8d43cf0463bcf0fc1224694d824171a7fde673e5
brake <riaconoo@icloud.com>
2023-02-18 15:29:07 +1100

    created test for warning page on no returned movies

--- geek-flix/src/app/home-card/home-card.component.spec.ts
+++ geek-flix/src/app/home-card/home-card.component.spec.ts
@@ -9,7 +9,6 @@ import {
 import { HomeCardComponent } from './home-card.component';
 
 fdescribe('HomeCardComponent', () => {
-
   let spectator: Spectator<HomeCardComponent>;
   const createComponent = createComponentFactory({
     component: HomeCardComponent,
@@ -51,22 +50,45 @@ fdescribe('HomeCardComponent', () => {
 
     // test that the image is there.
     expect(spectator.query('.thumbnail-img')).toHaveAttribute('src');
-
   });
 
-  xit('should show a message to the user when there are no movies', () => {
+  it('should show a message to the user when there are no movies', () => {
+    const movieDataService = spectator.inject(MovieDataService);
+    const movies = [
+      {
+        adult: true,
+        id: 1,
+        title: 'Blue Lagoon',
+        poster_path: '/d9nBoowhjiiYc4FBNtQkPY7c11H.jpg',
+        release_date: '2023.02.15',
+      } as IBasicMovie,
+    ];
+
+    movieDataService.getTopMovies.and.returnValue(
+      of({ results: movies } as IPaginatedMovies)
+    );
+
+    // act
+    spectator.detectChanges();
+
+    // assert
+    expect(spectator.component).toBeTruthy();
+
+    // test if warning message is shown
+    if (spectator.component.noMoooovies == true) {
+      expect(spectator.query('.home-warning')).toHaveText(
+        'Sorry, we currently cannot display any movies. Please try again later.'
+      );
+    } else {
+      expect(spectator.component.noMoooovies == false);
+    }
   });
 
-  xit('should allow a user to click on a movie and see the details component', () => {
-  });
+  xit('should allow a user to click on a movie and see the details component', () => {});
 
-  xit('should ', () => {
-  });
+  xit('should ', () => {});
 
-  xit('should ', () => {
-  });
+  xit('should ', () => {});
 
-  xit('should ', () => {
-  });
-
+  xit('should ', () => {});
 });
